470fb7b14a3c7cf9482fbe19f46031582a4fd137
camilo martinez <radek.camilo.martinez@gmail.com>
2016-03-10 11:22:53 +0100

    recogida de datos

--- app/src/main/java/com/sourcey/materiallogindemo/DatosActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/DatosActivity.java
@@ -1,7 +1,10 @@
 package com.sourcey.materiallogindemo;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,18 +14,21 @@ import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-/**
- * Created by r.martinez on 10/03/2016.
- */
 public class DatosActivity extends AppCompatActivity {
     private static final String TAG = "DatosActivity";
 
-    @Bind(R.id.etEdad) EditText etEdad;
-    @Bind(R.id.etPeso) EditText etPeso;
-    @Bind(R.id.etAltura) EditText etAltura;
-    @Bind(R.id.btNext) Button btNext;
-    @Bind(R.id.rbMan)  RadioButton rbMan;
-    @Bind(R.id.rbWoman)  RadioButton rbWoman;
+    @Bind(R.id.etEdad)
+    EditText etEdad;
+    @Bind(R.id.etPeso)
+    EditText etPeso;
+    @Bind(R.id.etAltura)
+    EditText etAltura;
+    @Bind(R.id.btNext)
+    Button btNext;
+    @Bind(R.id.rbMan)
+    RadioButton rbMan;
+    @Bind(R.id.rbWoman)
+    RadioButton rbWoman;
 
 
     @Override
@@ -30,44 +36,101 @@ public class DatosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos);
         ButterKnife.bind(this);
-        etEdad.setEnabled(true);
+
         btNext.setEnabled(false);
         etPeso.setEnabled(false);
         etAltura.setEnabled(false);
-        rbWoman.setEnabled(false);
         rbMan.setEnabled(false);
+        rbWoman.setEnabled(false);
+
+        etEdad.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+
+                etPeso.setEnabled(true);
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+
+        etPeso.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+
+                etAltura.setEnabled(true);
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        });
+
+        etAltura.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+
+                rbWoman.setEnabled(true);
+                rbMan.setEnabled(true);
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        });
+
+       /* etEdad.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                etPeso.setEnabled(true);
+                return false;
+            }
+        });*/
+
+       /* etPeso.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                etAltura.setEnabled(true);
+                return false;
+            }
+        });
+
+        etAltura.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                rbWoman.setEnabled(true);
+                rbMan.setEnabled(true);
+                return false;
+            }
+        });*/
 
         rbMan.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 rbWoman.setChecked(false);
-            }
-        });
-        rbWoman.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                rbMan.setChecked(false);
+                btNext.setEnabled(true);
             }
         });
 
-        if(etEdad.getText()!=null){
-            etAltura.setEnabled(true);
-            if(etAltura.getText()!=null){
-                etPeso.setEnabled(true);
-                if(etPeso.getText()!=null){
-                    rbWoman.setEnabled(true);
-                    rbMan.setEnabled(true);
-                    if(rbMan.isChecked()|| rbWoman.isChecked()){
-                        btNext.setEnabled(true);
-                    }
-                }
+
+        rbWoman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbMan.setChecked(false);
+                btNext.setEnabled(true);
+
             }
-        }
+        });
+
+
         btNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                Intent intent = new Intent(getApplicationContext(), AppActivity.class);
+                startActivity(intent);
             }
         });
     }
